694a0b043f017b79493411ea0ee924849e494b7f
join <join.zhou@topscode.com>
2019-06-19 17:06:44 +0800

    mp3文件属性信息处理

---
+++ src/main/java/com/join/tools/HttpClientTools.java
@@ -0,0 +1,153 @@
+/**
+ * 
+ */
+
+package com.join.tools;
+
+import org.apache.http.Consts;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.util.TextUtils;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+/**
+ * 
+ * HttpClientTools
+ * 
+ * @author join
+ *
+ */
+public class HttpClientTools {
+
+    private final static String defaultCharset = Consts.UTF_8.displayName();
+
+    private final static String MIME_TEXT = "text/plain";
+
+    private static CloseableHttpClient httpClient;
+
+    /**
+     * 取HTTPCLIENT
+     * 
+     * @return
+     */
+    private synchronized static CloseableHttpClient getHttpClient() {
+        if (null == httpClient) {
+            RequestConfig requestConfig =
+                RequestConfig.custom().setConnectTimeout(10000).setConnectionRequestTimeout(10000).setSocketTimeout(60000).build();
+            ConnectionSocketFactory plainsf = PlainConnectionSocketFactory.getSocketFactory();
+
+
+            SSLContext sslContext = null;
+            try {
+                sslContext = SSLContext.getInstance("SSL");
+                sslContext.init(null, new TrustManager[] {new X509TrustManager() {
+
+                    @Override
+                    public X509Certificate[] getAcceptedIssuers() {
+
+                        return null;
+                    }
+
+                    @Override
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
+
+                    }
+
+                    @Override
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+
+                    }
+                }}, new SecureRandom());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            SSLConnectionSocketFactory sslsf =
+                new SSLConnectionSocketFactory(sslContext, SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+
+            Registry<ConnectionSocketFactory> r =
+                RegistryBuilder.<ConnectionSocketFactory>create().register("http", plainsf).register("https", sslsf).build();
+
+
+            HttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(r);
+            httpClient = HttpClients.custom().setDefaultRequestConfig(requestConfig).setConnectionManager(cm).build();
+
+
+        }
+
+        return httpClient;
+
+    }
+
+    /**
+     * 发送get请求
+     * 
+     * @param url
+     * @return
+     * @throws ClientProtocolException
+     * @throws IOException
+     */
+    public static String doGet(String url) throws IOException {
+        if (TextUtils.isEmpty(url)) {
+            return null;
+        }
+
+        String retstr = null;
+        HttpGet get = new HttpGet(url);
+
+        try (CloseableHttpResponse response = getHttpClient().execute(get)) {
+            HttpEntity entity = response.getEntity();
+
+            if (null != entity) {
+                retstr = EntityUtils.toString(entity, defaultCharset);
+            }
+            EntityUtils.consume(entity);
+        }
+        return retstr;
+    }
+
+    /**
+     * 下载
+     *
+     * @param url
+     * @return
+     * @throws IOException
+     */
+    public static byte[] doGetDownload(String url) throws IOException {
+        if (TextUtils.isEmpty(url)) {
+            return null;
+        }
+
+        HttpGet get = new HttpGet(url);
+
+        try (CloseableHttpResponse response = getHttpClient().execute(get)) {
+            HttpEntity entity = response.getEntity();
+            if (entity == null) {
+                return null;
+            }
+
+            return EntityUtils.toByteArray(entity);
+        }
+
+    }
+
+}
---
+++ src/main/java/com/join/tools/mp3/Mp3Tools.java
@@ -0,0 +1,60 @@
+package com.join.tools.mp3;
+
+import com.mpatric.mp3agic.*;
+
+import java.io.File;
+
+/**
+ * mp3agic 一个修改mp3文件属性信息工具库
+ * https://github.com/mpatric/mp3agic
+ *
+ * @author ：Join
+ * @date ：Created in 2019/6/19 14:59
+ */
+public class Mp3Tools {
+
+	public static void main(String[] args) {
+
+		String dir="C:\\Users\\join0\\Downloads";
+		String destDir="C:\\Users\\join0\\Downloads\\dest\\";
+		File parent=new File(dir);
+		File[] files=parent.listFiles();
+
+		for (File file : files) {
+			if(file.getName().endsWith(".mp3")){
+				try {
+					Mp3File mp3file = new Mp3File(file);
+					ID3v1 id3v1Tag;
+
+					if (mp3file.hasId3v2Tag()) {
+						id3v1Tag =  mp3file.getId3v2Tag();
+					} else if(mp3file.hasId3v1Tag()){
+						id3v1Tag =  mp3file.getId3v1Tag();
+					} else {
+						// mp3 does not have an ID3v1 tag, let's create one..
+						id3v1Tag = new ID3v1Tag();
+						mp3file.setId3v1Tag(id3v1Tag);
+					}
+					//id3v1Tag.setTrack("5");
+					id3v1Tag.setArtist("纪涵邦");
+					//把mp3标题修改成与文件名一致,在音乐播放器列表中显示的是title,而不是文件名，可以根据自己的需求修改title
+					id3v1Tag.setTitle(file.getName());
+					id3v1Tag.setAlbum("汉朝那些事");
+					id3v1Tag.setYear("2019");
+					id3v1Tag.setGenre(12);
+					//id3v1Tag.setComment("Some comment");
+					mp3file.save(destDir+file.getName());
+
+					//System.out.println(file.getName()+ " process success: ");
+				} catch (Exception e) {
+					System.out.println(file.getName()+ " process fail: "+e.getLocalizedMessage());
+					//e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+
+
+}
